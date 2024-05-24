86ffee1c34bbed1f5ed435bde20d8ccdb1a47783
Nio4124 <antoniozottola41204@gmail.com>
2024-05-24 13:22:06 +0200

    creati i models

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
+
 from django.http import HttpResponse
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the library index.")
-
