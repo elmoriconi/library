4b0abd0c32b1f856e399c132cc7af67be74fd9a1
Nio4124 <antoniozottola41204@gmail.com>
2024-06-12 19:04:03 +0200

    convertite le classi in modelli

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -1,77 +1,45 @@
 from django.db import models
 
 class Book(models.Model):
-    
-    def __init__(self, book_id: str, title: str, author: str) -> None:
-        self.book_id: str = book_id
-        self.title: str = title
-        self.author: str = author
-        self.is_borrowed: bool = False
-        self.is_expired: bool = False
-        
+    book_id = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    is_borrowed = models.BooleanField(default=False)
+    is_expired = models.BooleanField(default=False)
+
     def __str__(self):
         return f"{self.book_id}, {self.title}, {self.author}, {self.is_borrowed}"
-        
-    def borrow(self):                       #if book not borrowed, turns is_borrowed to True
-        if not self.is_borrowed:
-            self.is_borrowed = True
-        
-    def return_book(self):                  #if book borrowed, turns is borrowed to False<
-        if self.is_borrowed:
-            self.is_borrowed = False
 
 class Member(models.Model):
-    
-    def __init__(self, member_id: str, name: str) -> None:
-        self.member_id: str = member_id
-        self.name: str = name
-        self.borrowed_books: list[Book] = []
-        
+    member_id = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    borrowed_books = models.ManyToManyField(Book)
+
     def __str__(self):
         return f"{self.member_id}, {self.name}, {self.borrowed_books}"
-    
-    def borrow_book(self, book: Book):
-        if book.is_borrowed:                 #if book already borrowed, raise error
-            raise ValueError("Book is already borrowed")
-        else:                                #else append book to member's list, then turns book.is_borrowed to True
-            self.borrowed_books.append(book)
-            book.borrow()
-    
-    def return_book(self, book: Book):
-        if book not in self.borrowed_books:  #if member's list doesn't contain book, raise error
-            raise ValueError("Book not borrowed by this member")
-        book.return_book()                   #else turns book.is_borrowed to False and removes it from member's list
-        self.borrowed_books.remove(book)
 
 class Library(models.Model):
-    
-    def __init__(self) -> None:
-        self.books: dict[str, Book] = {}
-        self.members: dict[str, Member] = {}
-        
-    def add_book(self, book_id: str, title: str, author: str):  
-        self.books[book_id] = Book(book_id, title, author)
-        
-    def register_member(self, member_id: str, name: str):
-        self.members[member_id] = Member(member_id, name)
-        
-    def borrow_book(self, member_id: str, book_id: str):
-        if book_id not in self.books:         #if book not in books dictionary, raise error
-            raise ValueError("Book not found")
-        if member_id not in self.members:     #if member not in members dictionary, raise error
-            raise ValueError("Member not found")
-        self.members[member_id].borrow_book(self.books[book_id])   #by calling function borrow_book, adds book to member's list and turns is_borrowed to True
-    
-    def return_book(self, member_id: str, book_id: str):
-        if book_id not in self.books:         #if book not in books dictionary, raise error
-            raise ValueError("Book not found")
-        if member_id not in self.members:     #if member not in members dictionary, raise error
-            raise ValueError("Member not found")
-        self.members[member_id].return_book(self.books[book_id])    #by calling function return_book, removes book from member's list and turns is_borrowed to False
-            
+    books = models.ManyToManyField(Book)
+    members = models.ManyToManyField(Member)
+
+    def add_book(self, book_id, title, author):
+        book = Book.objects.create(book_id=book_id, title=title, author=author)
+        self.books.add(book)
+
+    def register_member(self, member_id, name):
+        member = Member.objects.create(member_id=member_id, name=name)
+        self.members.add(member)
+
+    def borrow_book(self, member_id, book_id):
+        book = Book.objects.get(book_id=book_id)
+        member = Member.objects.get(member_id=member_id)
+        member.borrowed_books.add(book)
+
+    def return_book(self, member_id, book_id):
+        book = Book.objects.get(book_id=book_id)
+        member = Member.objects.get(member_id=member_id)
+        member.borrowed_books.remove(book)
+
     def get_borrowed_books(self, member_id):
-        if member_id in self.members:
-            member = self.members.get(member_id)    #gets member's info to access it's borrowed_books
-            return member.borrowed_books
-        else:
-            raise ValueError("Member not found")    #if member not in members dictionary, raise error
+        member = Member.objects.get(member_id=member_id)
+        return member.borrowed_books.all()
