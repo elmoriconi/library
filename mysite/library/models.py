721eb9bea2de78aa91fdf4c7e55a315568a4d64e
Nio4124 <antoniozottola41204@gmail.com>
2024-07-22 17:29:32 +0200

    cancellati un po' di file inutili

--- mysite/library/models.py
+++ mysite/library/models.py
@@ -3,42 +3,6 @@ from django.db import models
 class Library(models.Model):
     name = models.CharField(max_length=100, default = None, unique = True, primary_key=True)
 
-    """def add_library(self, name):
-        library = Library.objects.create(name)
-        library.save()
-
-    def add_book(self, book_id, title, author, library):
-        book = Book.objects.create(book_id=book_id, title=title, author=author)
-        self.library = library
-        book.save()
-        self.books.add(book)
-
-    def register_member(self, member_id, name, library):
-        member = Member.objects.create(member_id=member_id, name=name)
-        self.library = library
-        member.save()
-        self.members.add(member)
-
-    def borrow_book(self, member_id, book_id):
-        book = Book.objects.get(book_id=book_id)
-        member = Member.objects.get(member_id=member_id)
-        member.borrowed_books.add(book)
-
-    def return_book(self, member_id, book_id):
-        book = Book.objects.get(book_id=book_id)
-        member = Member.objects.get(member_id=member_id)
-        member.borrowed_books.remove(book)
-
-    def get_borrowed_books(self, member_id):
-        member = Member.objects.get(member_id=member_id)
-        return member.borrowed_books.all()
-    
-    @staticmethod
-    def get_library_of_member(member_id):
-        member = Member.objects.get(member_id=member_id)
-        library = Library.objects.filter(members=member).first()
-        return library"""
-
 class Member(models.Model):
     member_id = models.CharField(max_length=100, default = None)
     name = models.CharField(max_length=100, default = None)
